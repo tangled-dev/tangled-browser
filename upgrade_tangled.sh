35e52bf8ef16a2d8fcee1cb2bf607fcb17dc72de
Eriksson Monteiro <erikssonmonteiro@gmail.com>
2022-10-21 20:21:49 +0100

    update upgrade_tangled.sh

--- upgrade_tangled.sh
+++ upgrade_tangled.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 echo "working on chromium"
-#bash setup_chromium.sh $1
+bash setup_chromium.sh $1
 
 echo "working on millix"
 bash setup_nodejs.sh "$2-$3"
 
 echo "working on tangled advertisement"
-#bash setup_tangled_advertisement.sh
+bash setup_tangled_advertisement.sh
 
 echo "working on millix node"
-#bash setup_millix_node.sh $3
+bash setup_millix_node.sh $3
 
 echo "working on millix wallet ui"
-#bash setup_millix_wallet_ui.sh
+bash setup_millix_wallet_ui.sh
 
 echo "working on tangled"
-#bash setup_tangled.sh $2 $3
+bash setup_tangled.sh $2 $3
