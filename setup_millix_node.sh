1aa1c555b060e46d839520183bbcce2bfed1f7f8
Eriksson Monteiro <erikssonmonteiro@gmail.com>
2022-10-26 01:25:05 +0100

    use npm-cli.js on windows

--- setup_millix_node.sh
+++ setup_millix_node.sh
@@ -10,6 +10,6 @@ if [[ " ${NODEJS_DIST_UNIX[@]} " =~ " ${NODEJS_DIST} " ]]; then
     ../nodejs/bin/node ../nodejs/bin/npm install --scripts-prepend-node-path --build-from-source --target_arch=$1 --fallback-to-build
     ../nodejs/bin/node ../nodejs/bin/npm run dist --scripts-prepend-node-path
 else
-    ../nodejs/node ../nodejs/npm install --scripts-prepend-node-path --build-from-source --target_arch=$1 --fallback-to-build
-    ../nodejs/node ../nodejs/npm run dist --scripts-prepend-node-path
+    ../nodejs/node ../nodejs/node_modules/npm/bin/npm-cli.js install --scripts-prepend-node-path --build-from-source --target_arch=$1 --fallback-to-build
+    ../nodejs/node ../nodejs/node_modules/npm/bin/npm-cli.js run dist --scripts-prepend-node-path
 fi
--- setup_millix_wallet_ui.sh
+++ setup_millix_wallet_ui.sh
@@ -9,6 +9,6 @@ if [[ " ${NODEJS_DIST_UNIX[@]} " =~ " ${NODEJS_DIST} " ]]; then
   ../nodejs/bin/node ../nodejs/bin/npm install --scripts-prepend-node-path
   ../nodejs/bin/node ../nodejs/bin/npm run build --scripts-prepend-node-path
 else
-  ../nodejs/node ../nodejs/npm install --scripts-prepend-node-path
-  ../nodejs/node ../nodejs/npm run build --scripts-prepend-node-path
+  ../nodejs/node ../nodejs/node_modules/npm/bin/npm-cli.js install --scripts-prepend-node-path
+  ../nodejs/node ../nodejs/node_modules/npm/bin/npm-cli.js run build --scripts-prepend-node-path
 fi
