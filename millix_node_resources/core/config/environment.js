7cc8a650084f0af6cd51653c69e3867175cc08e5
Eriksson Monteiro <erikssonmonteiro@gmail.com>
2023-01-15 00:07:29 +0000

    update millix-node version 1.23.0

--- millix_node_resources/core/config/environment.js
+++ millix_node_resources/core/config/environment.js
@@ -1,4 +1,4 @@
-const NODE_MILLIX_VERSION        = '1.22.2-tangled';
+const NODE_MILLIX_VERSION        = '1.23.0-tangled';
 const DATA_BASE_DIR_MAIN_NETWORK = './millix-tangled';
 const DATA_BASE_DIR_TEST_NETWORK = './millix-tangled-testnet';
 
