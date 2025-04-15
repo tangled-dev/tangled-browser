bd9670eb164c22e79de927b3fe880ab48e7c0b50
Eriksson Monteiro <erikssonmonteiro@gmail.com>
2025-04-15 18:20:33 +0100

    add missing file from previous commit

--- millix_node_resources/core/config/environment.js
+++ millix_node_resources/core/config/environment.js
@@ -1,4 +1,4 @@
-const NODE_MILLIX_VERSION        = '1.23.3-tangled';
+const NODE_MILLIX_VERSION        = '1.24.0-tangled';
 const DATA_BASE_DIR_MAIN_NETWORK = './millix-tangled';
 const DATA_BASE_DIR_TEST_NETWORK = './millix-tangled-testnet';
 
