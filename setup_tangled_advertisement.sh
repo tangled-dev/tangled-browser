2580c3fba12e20c0d962998423c3450d2f3da825
Eriksson Monteiro <erikssonmonteiro@gmail.com>
2025-04-23 14:41:38 +0100

    add --arch to npm build script

--- setup_tangled_advertisement.sh
+++ setup_tangled_advertisement.sh
@@ -8,9 +8,9 @@ NODEJS_DIST_UNIX=("darwin" "linux")
 rm -rf node_modules
 
 if [[ " ${NODEJS_DIST_UNIX[@]} " =~ " ${NODEJS_DIST} " ]]; then
-  ../nodejs/bin/node ../nodejs/bin/npm install --scripts-prepend-node-path --build-from-source --target_arch=$2 --fallback-to-build
+  ../nodejs/bin/node ../nodejs/bin/npm install --scripts-prepend-node-path --build-from-source --arch=$2 --target_arch=$2 --fallback-to-build
   ../nodejs/bin/node ../nodejs/bin/npm run build --scripts-prepend-node-path
 else
-  ../nodejs/node ../nodejs/node_modules/npm/bin/npm-cli.js install --scripts-prepend-node-path --build-from-source --target_arch=$2 --fallback-to-build
+  ../nodejs/node ../nodejs/node_modules/npm/bin/npm-cli.js install --scripts-prepend-node-path --build-from-source --arch=$2 --target_arch=$2 --fallback-to-build
   ../nodejs/node ../nodejs/node_modules/npm/bin/npm-cli.js run build --scripts-prepend-node-path
 fi
