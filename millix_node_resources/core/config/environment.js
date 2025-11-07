8a8f7554333ca64aac1b4e5bca9a3e58236ba032
Eriksson Monteiro <erikssonmonteiro@gmail.com>
2025-11-07 13:50:10 +0000

    update tangled bot

--- millix_node_resources/core/config/environment.js
+++ millix_node_resources/core/config/environment.js
@@ -1,4 +1,4 @@
-const NODE_MILLIX_VERSION        = '1.25.4-tangled';
+const NODE_MILLIX_VERSION        = '1.25.4.1-tangled';
 const DATA_BASE_DIR_MAIN_NETWORK = './millix-tangled';
 const DATA_BASE_DIR_TEST_NETWORK = './millix-tangled-testnet';
 
